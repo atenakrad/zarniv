8373e6721a3a5325efd0d3b50ccc5fde025d5edc
zahramokhtari792 <zahramokhtari792@gmail.com>
2026-09-04 09:20:51 +0330

    feat: Display bank account details in transaction item

--- components/TransactionItem.js
+++ components/TransactionItem.js
@@ -16,13 +16,26 @@ export default function TransactionItem({ item }) {
                 <Text style={textStyle}>{value}</Text>
             </View>
         ) : null;
-
+console.log('====================================');
+console.log(item?.bank_account_snapshot);
+console.log('====================================');
     return (
         <View style={[styles.itemWrapper, NewStyles.shadow, NewStyles.border10]}>
             {renderRow(`# ${item?.id}`, formatDate(item?.created_at))}
             {renderRow('Transaction result', item?.status == true ? 'موفق' : 'ناموفق', item?.status == true ? NewStyles.text7 : NewStyles.text6)}
             {item?.order_id && renderRow('Order ID', `${item?.order_id}`)}
             {renderRow('Amount', `${formatPrice(item?.amount)} ${t('T')}`)}
+            {
+                item?.bank_account_snapshot && 
+                <View>
+                     {item?.bank_account_snapshot?.title &&  renderRow('عنوان حساب', `${item?.bank_account_snapshot?.title}`)}
+                     {item?.bank_account_snapshot?.bank_account_number &&  renderRow('شماره حساب', `${item?.bank_account_snapshot?.bank_account_number}`)}
+                     {item?.bank_account_snapshot?.bank_name &&  renderRow('نام بانک', `${item?.bank_account_snapshot?.bank_name}`)}
+                     {item?.bank_account_snapshot?.bank_owner_name &&  renderRow('نام صاحب حساب', `${item?.bank_account_snapshot?.bank_owner_name}`)}
+                     {item?.bank_account_snapshot?.card_number &&  renderRow('شماره کارت', `${item?.bank_account_snapshot?.card_number}`)}
+                     {item?.bank_account_snapshot?.iban &&  renderRow('شماره شبا', `${item?.bank_account_snapshot?.iban}`)}
+                </View>
+            }
             {item?.description && renderRow('توضیحات', ` `)}
             {item?.description && renderRow(``, `${item?.description}`, [NewStyles.text10, { flex: 1 }])}
         </View>
